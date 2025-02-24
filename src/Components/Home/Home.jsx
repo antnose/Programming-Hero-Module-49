faaf997d37212deb5b014312b74aa283a233ed2a
antnose <antnose28@gmail.com>
2025-02-25 01:12:44 +0600

    complete 2nd video of module 49

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>I am from Home.jsx</h1>
+    </div>
+  );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./Components/layouts/Main.jsx";
+import Home from "./Components/Home/Home.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
