7ffb300852a9138b43407b1169a498121410f52e
antnose <antnose28@gmail.com>
2025-02-26 10:29:58 +0600

    complete 6th video of module 49

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,13 +1,19 @@
-import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  GithubAuthProvider,
+} from "firebase/auth";
 import { auth } from "../../firebase/firebase.init";
 import { use, useState } from "react";
 
 const Login = () => {
   const [user, setUser] = useState(null);
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
   const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then((result) => {
         setUser(result.user);
       })
@@ -28,22 +34,35 @@ const Login = () => {
       });
   };
 
+  const handleGitHubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(`Error ${error}`);
+      });
+  };
+
   return (
     <div>
       {user ? (
-        <div>
-          <button onClick={handleGoogleSignIn} className="btn m">
-            Login with Google
-          </button>
-          <button className="btn">Login With GitHub</button>
-        </div>
-      ) : (
         <div>
           <h4 className="text-4xl"> Name: {user.displayName} </h4>
           <p className="text-2xl">Email: {user.email} </p>
           <img src={user.photoURL} alt="" />
           <button onClick={handleSignOut} className="btn btn-soft ml-4">
             Sign Out
+          </button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={handleGoogleSignIn} className="btn m">
+            Login with Google
+          </button>
+          <button onClick={handleGitHubSignIn} className="btn">
+            Login With GitHub
           </button>
         </div>
       )}
